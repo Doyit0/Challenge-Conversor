4ba5d045b9efe540fddcdc7da6dee787993321c8
Doyel Saulino Ochoa <saulinodoyel@gmail.com>
2023-03-19 18:07:12 -0300

    antes de api

--- src/Velocity.java
+++ src/Velocity.java
@@ -1,4 +1,13 @@
 public class Velocity {
-    private String distance;
-    private String time;
+    private String unit;
+    private String name;
+
+    public static final Velocity KM_PER_H =  new Velocity("km/h","Kilómetros por hora");
+    public static final Velocity M_PER_S =  new Velocity("m/s","Metro por segundo");
+    public static final Velocity M_PER_H =  new Velocity("km/h","Milla por hora");
+    public static final Velocity kmh =  new Velocity("km/h","Kilómetros por hora");
+    public static final Velocity kmh =  new Velocity("km/h","Kilómetros por hora");
+
+    public Velocity(String unit, String name) {
+    }
 }
